0602a37315fe1e524fdf187af2e441341c236213
franciscokataldocl <franciscolork@gmail.com>
2021-07-20 22:10:22 -0400

    redirect url not token

--- public/covid19/assets/js/loginValidation.js
+++ public/covid19/assets/js/loginValidation.js
@@ -14,6 +14,11 @@ const errorLogin = document.getElementById("errorLogin");
     situacionChile.innerHTML =
       '<a class="nav-link situacion" href="/covid19/situacion-chile.html">Situación en Chile</a>';
   } else {
+    const location = window.location.href;
+    if (location === "http://localhost:3000/covid19/situacion-chile.html") {
+      window.location.href = "/covid19/";
+    }
+
     itemSesion.innerHTML = `
     <a id="nav-item-login" class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Iniciar Sesión</a>`;
     situacionChile.innerHTML = "";
@@ -37,21 +42,20 @@ const loginValidation = async (email, password) => {
       errorLogin.innerHTML = "";
       location.reload();
     }
-
-    return token;
   } catch (err) {
     console.error(`Error: ${err}`);
   }
 };
 
 //form click
-loginButon.addEventListener("click", async (e) => {
+loginButon.addEventListener("click", (e) => {
   e.preventDefault();
   loginValidation(userEmail.value, userPassword.value);
 });
 
 //cerrar sesion
-cerrarSesion.addEventListener("click", async (e) => {
+//deberia instanciar esta funcion solo si se encuentra la llave en localstorage
+cerrarSesion.addEventListener("click", (e) => {
   localStorage.removeItem("llave");
   window.location.href = "/covid19/";
 });
